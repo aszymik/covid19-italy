ede982796477dda7c8891df5b6c9a3f4b5da5bb4
annnie73 <aniaszymik02@gmail.com>
2025-05-19 16:24:52 +0200

    change sirs parameters

--- scripts/daily_deaths.py
+++ scripts/daily_deaths.py
@@ -8,6 +8,7 @@ df['data'] = pd.to_datetime(df['data'])
 df_daily = df.groupby('data', as_index=False)[['deceduti']].sum()
 df_daily['nuovi_deceduti'] = df_daily['deceduti'].diff()
 
+# Plot deaths
 fig = px.line(df_daily, x='data', y='nuovi_deceduti',
                          title='Daily New Deaths in Italy',
                          labels={'data': 'Date', 'nuovi_deceduti': 'New Deaths'})
--- scripts/total_cases.py
+++ scripts/total_cases.py
@@ -7,8 +7,9 @@ df['data'] = pd.to_datetime(df['data'])
 # Group by date (aggregate all regions)
 df_daily = df.groupby('data', as_index=False)[['totale_casi']].sum()
 
+# Plot total cases
 fig = px.line(df_daily, x='data', y='totale_casi',
               title='Total Coronavirus Cases in Italy',
-              labels={'data': 'Date', 'totale_casi': 'Total Cases'})
+              labels={'data': 'Date', 'totale_casi': 'Total Coronavirus Currently Infected'})
 
 fig.write_html('plots/total_cases.html')
