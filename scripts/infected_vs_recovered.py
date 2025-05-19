151f2ccded8ac92230bead3f687b0bfdb596c14b
annnie73 <aniaszymik02@gmail.com>
2025-05-19 14:00:48 +0200

    change legend

--- scripts/infected_vs_recovered.py
+++ scripts/infected_vs_recovered.py
@@ -6,11 +6,16 @@ df['data'] = pd.to_datetime(df['data'])
 
 # Group by date (aggregate all regions)
 df_daily = df.groupby('data', as_index=False)[['nuovi_positivi', 'dimessi_guariti']].sum()
-df_daily['nuovi_guariti'] = df_daily['dimessi_guariti'].diff()
+
+# Create new columns
+df_daily['New Recoveries'] = df_daily['dimessi_guariti'].diff()
+df_daily = df_daily.rename(columns={
+    'nuovi_positivi': 'New Cases',
+})
 
 fig = px.line(
-    df_daily, x='data', y=['nuovi_positivi', 'nuovi_guariti'],
+    df_daily, x='data', y=['New Cases', 'New Recoveries'],
     title='Newly Infected vs. Newly Recovered in Italy',
-    labels={'value': 'New Daily Coronavirus Cases + Cured', 'variable': 'Category', 'data': 'Date'}
+    labels={'value': 'New Daily Coronavirus Cases + Cured', 'variable': 'Group', 'data': 'Date'}
 )
 fig.show()
