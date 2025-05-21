83072267ef5f5ffa3d20ab7b19fce35874d31459
annnie73 <aniaszymik02@gmail.com>
2025-05-21 12:22:18 +0200

    add moving average

--- scripts/daily_cases.py
+++ scripts/daily_cases.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import plotly.express as px
+import plotly.graph_objects as go
 
 df = pd.read_csv('data/dpc-covid19-ita-regioni.csv')
 df['data'] = pd.to_datetime(df['data'])
@@ -7,9 +7,37 @@ df['data'] = pd.to_datetime(df['data'])
 # Group by date (aggregate all regions)
 df_daily = df.groupby('data', as_index=False)[['nuovi_positivi']].sum()
 
-# Plot new cases
-fig = px.line(df_daily, x='data', y='nuovi_positivi',
-                    title='Daily New Cases in Italy',
-                    labels={'data': 'Date', 'nuovi_positivi': 'New Cases'})
+# Compute 7-day moving average
+df_daily['ma_7'] = df_daily['nuovi_positivi'].rolling(window=7, center=True).mean()
+
+fig = go.Figure()
+
+# Daily new cases
+fig.add_trace(go.Scatter(
+    x=df_daily['data'],
+    y=df_daily['nuovi_positivi'],
+    mode='lines',
+    name='Daily New Cases',
+    hovertemplate='New Cases: %{y:.5s}<extra></extra>'
+))
+
+# 7-day moving average
+fig.add_trace(go.Scatter(
+    x=df_daily['data'],
+    y=df_daily['ma_7'],
+    mode='lines',
+    name='7-Day Moving Average',
+    line=dict(color='orange'),
+    hovertemplate='7-Day Average: %{y:.5s}<extra></extra>'
+))
+
+# Customize layout
+fig.update_layout(
+    title='Daily New COVID-19 Cases in Italy',
+    xaxis_title='Date',
+    yaxis_title='New Cases',
+    legend_title='Legend',
+    hovermode='x unified',
+)
 
 fig.write_html('plots/daily_cases.html')
--- scripts/daily_deaths.py
+++ scripts/daily_deaths.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import plotly.express as px
+import plotly.graph_objects as go
 
 df = pd.read_csv('data/dpc-covid19-ita-regioni.csv')
 df['data'] = pd.to_datetime(df['data'])
@@ -8,9 +8,38 @@ df['data'] = pd.to_datetime(df['data'])
 df_daily = df.groupby('data', as_index=False)[['deceduti']].sum()
 df_daily['nuovi_deceduti'] = df_daily['deceduti'].diff()
 
-# Plot deaths
-fig = px.line(df_daily, x='data', y='nuovi_deceduti',
-                         title='Daily New Deaths in Italy',
-                         labels={'data': 'Date', 'nuovi_deceduti': 'New Deaths'})
+# Compute 7-day moving average
+df_daily['ma_7'] = df_daily['nuovi_deceduti'].rolling(window=7, center=True).mean()
 
+fig = go.Figure()
+
+# Daily deaths
+fig.add_trace(go.Scatter(
+    x=df_daily['data'],
+    y=df_daily['nuovi_deceduti'],
+    mode='lines',
+    name='Daily Deaths',
+    hovertemplate='New Deaths: %{y:.0f}<extra></extra>'
+))
+
+# 7-day moving average
+fig.add_trace(go.Scatter(
+    x=df_daily['data'],
+    y=df_daily['ma_7'],
+    mode='lines',
+    name='7-Day Moving Average',
+    line=dict(color='orange'),
+    hovertemplate='7-Day Average: %{y:.4s}<extra></extra>'
+))
+
+# Customize layout
+fig.update_layout(
+    title='Daily Deaths in Italy',
+    xaxis_title='Date',
+    yaxis_title='Number of Deaths',
+    legend_title='Legend',
+    hovermode='x unified',
+)
+
+fig.show()
 fig.write_html('plots/daily_deaths.html')
